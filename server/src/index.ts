54c317e5b9f7e0a7611d8421642c8af5b5d9379f
RodrigoMonteroM <rodrigomontero240303@gmail.com>
2025-05-22 16:55:55 +0200

    creating server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import server from "./server.js";
+import * as colors from 'colors';
+
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(colors.cyan(`Rest Api Server listening on port ${port}`));
 })
+
+
 

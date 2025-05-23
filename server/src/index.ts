11dfdf57e8e2c86a3c996b9ca5e1ef1104ea6616
RodrigoMonteroM <rodrigomontero240303@gmail.com>
2025-05-23 15:34:34 +0200

    working on prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,16 @@ import server from './server.js';
 
 const port = process.env.PORT || 4000;
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+async function main(){
+    try{
+        server.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    }catch (error) {
+        console.error('Error initializing the server:', error);
+        process.exit(1);
+    }
+
+}
+
+main();

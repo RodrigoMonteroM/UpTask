8baae6165016140ab4e9bbb0cb93541459b0d152
RodrigoMonteroM <rodrigomontero240303@gmail.com>
2025-05-23 16:43:29 +0200

    test routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import projectRouter from "./router/projectRouter.js";
 
 const app = express();
 
+app.use(express.json());
+
+//ROUTES
+app.use('/api', projectRouter)
 export default app;
